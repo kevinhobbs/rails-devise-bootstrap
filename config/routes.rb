e5cc033632d85ef0d6e8e42596e5110d25c8ed30
Kevin Hobbs <jh1463-air-m1@gmail.com>
2023-12-24 15:56:01 -0800

    After Edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root 'pages#home'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
